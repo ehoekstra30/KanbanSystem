fde03d79839f89a9d18e6695cdd1dee1e0ab07b6
Ethan Hoekstra <Ehoekstra5571@conestogac.on.ca>
2020-11-11 16:02:05 -0500

    UI added

--- ConfigTool/MainWindow.xaml.cs
+++ ConfigTool/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +22,20 @@ namespace ConfigTool
     /// </summary>
     public partial class MainWindow : Window
     {
+        static private string dbName = "kanbandb";
+
         public MainWindow()
         {
             InitializeComponent();
+
+            string constr = ConfigurationManager.ConnectionStrings["kanban"].ConnectionString;
+
+            try {
+                SqlConnection connection = new SqlConnection("data source= .;database=Index_demo;integrated security=SSPI");
+            }
+            catch (Exception ex) {
+                MessageBox.Show("Cannot connect to " + dbName + "database\n" + ex.ToString(), "Error");
+            }
         }
     }
 }
