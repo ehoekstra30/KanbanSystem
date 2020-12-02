1ac8b5f23094234c86f9bbfc29e73f5ee856cc46
Mohamed Benzreba <mbenzreba7150@conestogac.on.ca>
2020-12-02 12:43:53 -0500

    add: Worker class to simulate worker at a workstation

---
+++ WorkstationSimulator/Worker.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+/*  FILE        : Worker.cs
+ *  PROJECT     : AdvSql Project
+ *  DEVELOPERS  : Ethan Hoekstra & Mohamed Benzreba
+ *  PURPOSE     : Models a worker at a Workstation, putting together foglamps.
+ */
+
+
+namespace WorkstationSimulator
+{
+    public enum ExperienceLevel_t
+    {
+        Rookie,
+        Experienced,
+        Senior
+    }
+
+    public class Worker
+    {
+        private ExperienceLevel_t experienceLevel;
+
+        public Worker(ExperienceLevel_t experienceLevel)
+        {
+            this.experienceLevel = experienceLevel;
+        }
+    }
+}
