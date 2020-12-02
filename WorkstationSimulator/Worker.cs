6db2ca1cb54dfb9b981193bef21039d0233b4dbe
Mohamed Benzreba <mbenzreba7150@conestogac.on.ca>
2020-12-02 15:48:04 -0500

    add: simulation features including working with DbContext for eventual connection to db

--- WorkstationSimulator/Worker.cs
+++ WorkstationSimulator/Worker.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using KanbanCore;
+
 
 /*  FILE        : Worker.cs
  *  PROJECT     : AdvSql Project
@@ -27,11 +29,13 @@ namespace WorkstationSimulator
         private int minutesPerSecond;
         private double fogLampsToMake;
         private int fogLampsMade;
+        private int fogLampsOnTestTray;
+        private int currentTestTrayId;
 
         private Random rand;
         private double assemblyTimeVariance;
 
-        public Worker(ExperienceLevel_t experienceLevel, int minutesPerSecond)
+        public Worker(KanbanDbModel kdb, ExperienceLevel_t experienceLevel, int minutesPerSecond)
         {
             this.experienceLevel = experienceLevel;
             this.minutesPerSecond = minutesPerSecond;
@@ -67,8 +71,29 @@ namespace WorkstationSimulator
             while (this.fogLampsToMake >= 1)
             {
                 // if there are parts available, make a foglamp
+                if (this.arePartsAvailable())
+                {
+                    // Update our counts
+                    this.fogLampsMade++;
+                    this.fogLampsToMake--;
 
+                    // Update the bins
+                    this.harnessBin--;
+                    this.reflectorBin--;
+                    this.housingBin--;
+                    this.lensBin--;
+                    this.bulbBin--;
+                    this.bezelBin--;
+                }
+                else
+                {
+                    // No parts available; break out of simulate loop
+                    break;
+                }
             }
+
+            // Update these newly made fog lamps to Db Model
+
         }
 
 
@@ -97,12 +122,12 @@ namespace WorkstationSimulator
 
         private bool arePartsAvailable()
         {
-            if (() &&
-                () &&
-                () &&
-                () &&
-                () &&
-                ())
+            if ((this.harnessBin > 0) &&
+                (this.reflectorBin > 0) &&
+                (this.housingBin > 0) &&
+                (this.lensBin > 0) &&
+                (this.bulbBin > 0) &&
+                (this.bezelBin > 0))
             {
                 return true;
             }
@@ -110,6 +135,44 @@ namespace WorkstationSimulator
             {
                 return false;
             }
+        }
+
+        private bool simulateWorkOnDb(KanbanDbModel kdb)
+        {
+            // For each fogLampMade...
+            // ...
+
+            // Get a hold of the current test tray we are working on
+            TestTray tt = kdb.TestTrays.Find(this.currentTestTrayId);
+            
+
+            if (tt.IsCompleted)
+            {
+                // Tray is complete and we are still waiting on runner; return
+                return true;
+            }
+
+            int nextFogLampId = tt.FogLamps.Count + 1;
+            FogLamp fl = new FogLamp();
+            fl.ExperienceLevelOfAssembler = (int)this.experienceLevel;
+            fl.TestTrayId = this.currentTestTrayId;
+
+            while ((nextFogLampId <= 60) && (this.fogLampsMade > 0))
+            {
+                fl.FogLampId = nextFogLampId;
+                kdb.FogLamps.Add(fl);
+
+                nextFogLampId++;
+                if (nextFogLampId == 61)
+                {
+                    // Mark tray as complete
+                    tt.IsCompleted = true;
+                }
+            }
+
+
+            kdb.SaveChanges();
+            return true;
         }
     }
 }
