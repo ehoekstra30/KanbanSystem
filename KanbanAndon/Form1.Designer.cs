15bd1a73466afdaccfeb4d18a92e44141333bf08
Ethan Hoekstra <Ehoekstra5571@conestogac.on.ca>
2020-12-17 18:42:50 -0500

    Andon qol

--- KanbanAndon/Form1.Designer.cs
+++ KanbanAndon/Form1.Designer.cs
@@ -31,38 +31,60 @@
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.progressBar1 = new System.Windows.Forms.ProgressBar();
             this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(13, 13);
+            this.dataGridView1.Location = new System.Drawing.Point(13, 61);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(744, 159);
+            this.dataGridView1.Size = new System.Drawing.Size(744, 104);
             this.dataGridView1.TabIndex = 0;
             // 
             // progressBar1
             // 
-            this.progressBar1.Location = new System.Drawing.Point(13, 191);
+            this.progressBar1.Location = new System.Drawing.Point(12, 184);
             this.progressBar1.Name = "progressBar1";
             this.progressBar1.Size = new System.Drawing.Size(744, 23);
             this.progressBar1.TabIndex = 1;
+            this.progressBar1.Click += new System.EventHandler(this.progressBar1_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(10, 175);
+            this.label1.Location = new System.Drawing.Point(9, 168);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(95, 13);
             this.label1.TabIndex = 2;
             this.label1.Text = "Runner Frequency";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(13, 34);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(104, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Workstation Number";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(769, 219);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.progressBar1);
             this.Controls.Add(this.dataGridView1);
@@ -82,6 +104,8 @@
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.ProgressBar progressBar1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- KanbanAndon/Form1.cs
+++ KanbanAndon/Form1.cs
@@ -27,7 +27,7 @@ namespace KanbanAndon
         private WorkstationReader workstationReader;
         private Thread thread;
         private Boolean run;
-
+        private static int maxWorkStations = 3;
         private KanbanDbModel kdb;
 
         //delegate for updating the datagrid
@@ -41,6 +41,12 @@ namespace KanbanAndon
             this.Text = "Kanban Andon Display";
             this.FormBorderStyle = FormBorderStyle.FixedSingle;
             run = false;
+
+            int i = 1;
+            while (i <= maxWorkStations) {
+                    comboBox1.Items.Add(i).ToString();
+                    i++;
+                }
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -70,15 +76,19 @@ namespace KanbanAndon
             int runner = 0;
             progressBar1.Minimum = 0;
             progressBar1.Maximum = 5;
-
+            
             while (run == true)
             {
                 dataGridView1.BeginInvoke(new UpdateGridDelegate(UpdateGrid));
                 Thread.Sleep(1000);
-                progressBar1.Value = runner;
-                runner++;
-                if (runner > 5) {
-                    runner = 0;                
+                if (comboBox1.SelectedIndex != -1)
+                {
+                    progressBar1.Value = runner;
+                    runner++;
+                    if (runner > 5)
+                    {
+                        runner = 0;
+                    }
                 }
             }
 
@@ -88,21 +98,33 @@ namespace KanbanAndon
         //updates the chart with new bin values
         public void UpdateGrid() {
 
-            dataGridView1.Rows.Clear();
-
-            int i = 0;
-            List<Workstation> l = workstationReader.GetWorkstations();
-            foreach (Workstation workstation in l)
+            if (comboBox1.SelectedIndex != -1)
             {
-                dataGridView1.Rows.Add((i + 1).ToString(), workstation.HarnessAmount, workstation.ReflectorAmount, 
-                    workstation.HousingAmount, workstation.LensAmount, workstation.BulbAmount, workstation.BezelAmount) ;
+                dataGridView1.Rows.Clear();
+
+                int i = 0;
+                List<Workstation> l = workstationReader.GetWorkstations();
+                foreach (Workstation workstation in l)
+                {
+                    if (comboBox1.SelectedIndex == (i))
+                    {
+                        dataGridView1.Rows.Add((i + 1).ToString(), workstation.HarnessAmount, workstation.ReflectorAmount,
+                            workstation.HousingAmount, workstation.LensAmount, workstation.BulbAmount, workstation.BezelAmount);
+                    }
+                    i++;
+                }
+                dataGridView1.Show();
             }
-            dataGridView1.Show();
         }
 
         private void Form1_Closing(object sender, System.ComponentModel.CancelEventArgs e)
         {
             thread.Abort();
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
